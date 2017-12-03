e9539bb0a904b4a7bc0c518d64982b9a315531fe
Léo Dubus <dubusster@gmail.com>
2017-12-03 13:01:58 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+
+window.onload = function() {
+
+    var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create });
+
+    function preload () {
+
+
+    }
+
+    function create () {
+
+    }
+
+};
