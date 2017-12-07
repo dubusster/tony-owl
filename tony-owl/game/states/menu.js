9a4ce61b529dd9c4e588d8b9e9dcea08d8e01ef2
dubusster <dubusster@gmail.com>
2017-12-07 17:42:18 +0100

    updated display with group anchoring

--- tony-owl/game/states/menu.js
+++ tony-owl/game/states/menu.js
@@ -9,23 +9,28 @@ Menu.prototype = {
   create: function() {
 	
     // add the background sprite
-    this.background = this.game.add.tileSprite(0, 0,this.game.width, this.game.height, 'background');
+    this.background = this.add.tileSprite(0, 0,this.game.width, this.game.height, 'background');
     this.background.autoScroll(-100, 0);
 	  
 	this.titleGroup = this.game.add.group();
     
 	// adding the logo
-    this.logo = this.game.add.image(this.background.width/2, this.background.height/4,'logo');
-    this.logo.anchor.setTo(0.5, 0.5);
-    this.logo.scale.setTo(0.5,0.5);
+    this.logo = this.add.image(0, 0,'logo');
+    this.logo.scale.setTo(0.5);
+    
     
     // adding to global title group, so it is easily manipulable.
     this.titleGroup.add(this.logo);
     
     // adding title image
-    this.title = this.game.add.image(this.background.width/2, 3*this.background.height/4,'title');
-    this.title.anchor.setTo(0.5, 0.5);
+    this.title = this.add.image(0, 0,'title');
     this.titleGroup.add(this.title);
+    
+    this.titleGroup.align(1,2,this.logo.height, this.logo.height);
+    this.titleGroup.setAll('anchor.x', 0.5);
+    this.titleGroup.setAll('anchor.y', 0.5);
+    this.titleGroup.x = this.world.width/2;
+    this.titleGroup.y = this.world.height/4;
   },
   update: function() {
     
