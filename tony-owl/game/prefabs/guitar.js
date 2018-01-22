c2ff567b8a2a193800aa32a3b06a49bc02f03b95
dubusster <dubusster@gmail.com>
2018-01-22 18:41:36 +0100

    added basic throwable objects

---
+++ tony-owl/game/prefabs/guitar.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var Guitar = function(game, x, y, frame) {
+  Phaser.Sprite.call(this, game, x, y, 'guitar', frame);
+
+  	//enable physics on the enemy sprite
+	// this is needed for collision detection.
+	this.game.physics.arcade.enableBody(this);
+	// Guitars just flow through the screen as motherfucking shurikens
+	this.body.allowGravity = false;
+  
+};
+
+Guitar.prototype = Object.create(Phaser.Sprite.prototype);
+Guitar.prototype.constructor = Guitar;
+
+Guitar.prototype.update = function() {
+  
+  // write your prefab's specific update code here
+  
+};
+
+module.exports = Guitar;
