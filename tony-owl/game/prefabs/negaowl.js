cd41ea3109beba992009ec92166c5df103d9a576
dubusster <dubusster@gmail.com>
2018-01-31 15:18:23 +0100

    fixed bug boss killing by jumping on it

--- tony-owl/game/prefabs/negaowl.js
+++ tony-owl/game/prefabs/negaowl.js
@@ -1,21 +1,25 @@
 'use strict';
 
 var Negaowl = function(game, x, y, frame) {
-  Phaser.Sprite.call(this, game, x, y, 'negaowl', frame);
+	Phaser.Sprite.call(this, game, x, y, 'negaowl', frame);
 
-  // TODO: add boss animation
-  
-  this.game.physics.arcade.enableBody(this);
-  
+	// TODO: add boss animation
+
+	game.physics.arcade.enableBody(this);
+
 };
 
 Negaowl.prototype = Object.create(Phaser.Sprite.prototype);
 Negaowl.prototype.constructor = Negaowl;
 
+var MIN_THROWING_DELAY = 5000;
+var MAX_THROWING_DELAY = 10000;
 Negaowl.prototype.update = function() {
-  
-  // write your prefab's specific update code here
-  
+
 };
+
+Negaowl.prototype.shoot = function() {
+	console.log("throwing stuffs !");
+}
 
 module.exports = Negaowl;
--- tony-owl/game/states/play.js
+++ tony-owl/game/states/play.js
@@ -16,20 +16,23 @@ Play.prototype = {
 		var level_width = 2000;
 		this.game.world.setBounds(0, 0, level_width, 600);
 		this.autoscroll_speed = 30;
-		
+
 		// Generating backgrounds and landscape
-		this.sky = this.add.tileSprite(0, 0, this.game.world.width, this.game.height, 'sky');
-		this.background = this.add.tileSprite(0, 0, this.game.world.width, this.game.height, 'background');
+		this.sky = this.add.tileSprite(0, 0, this.game.world.width,
+				this.game.height, 'sky');
+		this.background = this.add.tileSprite(0, 0, this.game.world.width,
+				this.game.height, 'background');
 		this.sky.autoScroll(-30, 0);
-		
+
 		var ground_height = 50;
-		this.ground = new Ground(this.game, 0, this.game.height-ground_height, level_width, ground_height);
+		this.ground = new Ground(this.game, 0,
+				this.game.height - ground_height, level_width, ground_height);
 		this.game.add.existing(this.ground);
-		
+
 		// adding owl (player) to game
 		this.owl = new Owl(this.game, 100, this.game.height - 150);
 		this.game.add.existing(this.owl);
-		this.game.camera.follow(this.owl); 
+		this.game.camera.follow(this.owl);
 
 		// keep the spacebar from propagating up to the browser
 		this.game.input.keyboard.addKeyCapture([ Phaser.Keyboard.SPACEBAR,
@@ -38,59 +41,62 @@ Play.prototype = {
 		// add keyboard controls
 		var shootKey = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 		shootKey.onDown.add(this.owl.trick, this.owl);
-		
+
 		// add boss at the end of the map
-		this.boss = new Negaowl(this.game, this.game.world.width-100, this.game.height - 200);
+		this.boss = new Negaowl(this.game, this.game.world.width - 100,
+				this.game.height - 200);
 		this.game.add.existing(this.boss);
-//		
 		
+		var MIN_THROWING_DELAY = 3000;
+		var MAX_THROWING_DELAY = 5000;
+
+		// Send another thing soon
+
 
 	},
 	update : function() {
-		
-		var hit_platform = this.game.physics.arcade.collide(this.owl, this.ground);
+
+		var hit_platform = this.game.physics.arcade.collide(this.owl,
+				this.ground);
 		this.game.physics.arcade.collide(this.boss, this.ground);
 		this.game.physics.arcade.collide(this.owl, this.boss, touchingBoss);
-		
-		if (this.owl.body.position.x < 0) {		
+
+		if (this.owl.body.position.x < 0) {
 			this.owl.body.position.x = 0;
+		} else if (this.owl.body.position.x > this.game.world.width
+				- this.owl.body.width) {
+			this.owl.body.position.x = this.game.world.width
+					- this.owl.body.width;
 		}
-		else if (this.owl.body.position.x > this.game.world.width-this.owl.body.width) {
-			this.owl.body.position.x = this.game.world.width-this.owl.body.width;
-		}
-		
-		
+
 		// Player moves
 		var cursors = this.game.input.keyboard.createCursorKeys();
-		
+
 		if (cursors.right.isDown) {
 			this.owl.move("RIGHT");
-//			this.background.autoScroll(-100, 0);
-//			this.sky.autoScroll(-50, 0);
-		} else if (cursors.left.isDown) {		
-				this.owl.move("LEFT");
+		} else if (cursors.left.isDown) {
+			this.owl.move("LEFT");
 		} else {
-//			this.background.autoScroll(-50, 0);
 			this.owl.move(null);
-//			this.owl.body.velocity.x = -this.autoscroll_speed;
-			
+
 		}
-		
+
 		if (cursors.up.isDown && this.owl.body.touching.down) {
 			this.owl.move("UP");
 		}
+		
+		
 
 	},
 };
 
 function touchingBoss(player, enemy) {
-	if (player.body.touching.right || player.body.touching.left)
-    {
-        // player is dead
+	if (player.body.touching.right || player.body.touching.left || player.body.touching.up) {
+		// boss is dead
 		console.log('WIIIIIN');
-        player.game.state.start('win');
-    }
-	
+		player.game.state.start('win');
+	}
+
 }
 
 module.exports = Play;
