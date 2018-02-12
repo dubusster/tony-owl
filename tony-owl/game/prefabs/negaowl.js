760e19754d38add613d885882138a8e97ed639aa
dubusster <dubusster@gmail.com>
2018-02-12 15:42:58 +0100

    killing particle when out of bound world

--- tony-owl/game/prefabs/negaowl.js
+++ tony-owl/game/prefabs/negaowl.js
@@ -7,7 +7,7 @@ var PARTICLES_AMPLI = 15;
 var GUITAR_PER_ROW_MAX = 20;
 
 var GUITAR_LIFESPAN = 0;
-var AMPLI_LIFESPAN = 10000;
+var AMPLI_LIFESPAN = 0;
 
 var THROWING_VELOCITY_AMPLI_MIN = -300;
 var THROWING_VELOCITY_AMPLI_MAX = -800;
@@ -26,60 +26,78 @@ var Negaowl = function(game, x, y, frame) {
 
 	this.animations.add('standing', null, 10, true);
 	this.animations.play('standing');
-	
+
 	// ampli emitter
-    this.ampliEmitter = this.game.add.emitter(this.position.x, 2*this.height/3, PARTICLES_AMPLI);
-    this.ampliEmitter.height = 100;
-    this.ampliEmitter.particleClass = AmpliParticle;
-    this.ampliEmitter.makeParticles('ampli',0,PARTICLES_AMPLI, true);
-    this.ampliEmitter.minParticleSpeed.set(THROWING_VELOCITY_AMPLI_MIN, 0);
+	this.ampliEmitter = this.game.add.emitter(this.position.x,
+			2 * this.height / 3, PARTICLES_AMPLI);
+	this.ampliEmitter.height = 100;
+	this.ampliEmitter.particleClass = AmpliParticle;
+	this.ampliEmitter.makeParticles('ampli', 0, PARTICLES_AMPLI, true);
+	this.ampliEmitter.minParticleSpeed.set(THROWING_VELOCITY_AMPLI_MIN, 0);
 	this.ampliEmitter.maxParticleSpeed.set(THROWING_VELOCITY_AMPLI_MAX, 0);
 	this.ampliEmitter.gravity = 1200;
 	this.ampliEmitter.minRotation = 0;
-    this.ampliEmitter.maxRotation = 0;
-    
-    
-    this.guitarUp = new Guitar(THROWING_VELOCITY_GUITAR_MIN, THROWING_VELOCITY_GUITAR_MAX, this.game,this.position.x, 100, GUITAR_PER_ROW_MAX);
-	this.guitarMiddle = new Guitar(THROWING_VELOCITY_GUITAR_MIN, THROWING_VELOCITY_GUITAR_MAX, this.game,this.position.x, 250, GUITAR_PER_ROW_MAX);
-	this.guitarDown = new Guitar(THROWING_VELOCITY_GUITAR_MIN, THROWING_VELOCITY_GUITAR_MAX, this.game,this.position.x, 400, GUITAR_PER_ROW_MAX);
+	this.ampliEmitter.maxRotation = 0;
+
+	this.ampliEmitter.forEach(killParticle, this);
+
+	this.guitarUp = new Guitar(THROWING_VELOCITY_GUITAR_MIN,
+			THROWING_VELOCITY_GUITAR_MAX, this.game, this.position.x, 100,
+			GUITAR_PER_ROW_MAX);
+	this.guitarMiddle = new Guitar(THROWING_VELOCITY_GUITAR_MIN,
+			THROWING_VELOCITY_GUITAR_MAX, this.game, this.position.x, 250,
+			GUITAR_PER_ROW_MAX);
+	this.guitarDown = new Guitar(THROWING_VELOCITY_GUITAR_MIN,
+			THROWING_VELOCITY_GUITAR_MAX, this.game, this.position.x, 400,
+			GUITAR_PER_ROW_MAX);
 
 	this.guitarGroup = this.game.add.group();
-    this.guitarGroup.add(this.guitarUp.emitter);
-    this.guitarGroup.add(this.guitarMiddle.emitter);
-    this.guitarGroup.add(this.guitarDown.emitter);
-    
-    
+	this.guitarGroup.add(this.guitarUp.emitter);
+	this.guitarGroup.add(this.guitarMiddle.emitter);
+	this.guitarGroup.add(this.guitarDown.emitter);
+	
+	this.guitarGroup.forEach(EmitterKillOutOfBound, this);
+
 };
 
 Negaowl.prototype = Object.create(Phaser.Sprite.prototype);
 Negaowl.prototype.constructor = Negaowl;
 
 Negaowl.prototype.update = function() {
-	
+
 };
 
-Negaowl.prototype.release_hell = function(){
+Negaowl.prototype.release_hell = function() {
 	for (var i = 0; i < this.guitarGroup.children.length; i++) {
 		var guitar = this.guitarGroup.children[i];
-		guitar.start(false, GUITAR_LIFESPAN, 10000);	
+		guitar.start(false, GUITAR_LIFESPAN, 10000);
 	}
 	this.ampliEmitter.start(false, AMPLI_LIFESPAN, 2000);
 };
 
-Negaowl.prototype.change_emitters_frequencies = function(min_speed, max_speed){
+Negaowl.prototype.change_emitters_frequencies = function(min_speed, max_speed) {
 	for (var i = 0; i < this.guitarGroup.children.length; i++) {
 		var guitar = this.guitarGroup.children[i];
-		this.game.time.events.loop(1000,
-				randomEmitterFrequency, this, this.guitarUp.emitter, min_speed, max_speed);
+		this.game.time.events.loop(1000, randomEmitterFrequency, this,
+				this.guitarUp.emitter, min_speed, max_speed);
 	}
 
 	this.ampliLoopTimer = this.game.time.events.loop(1000,
-			randomEmitterFrequency, this, this.ampliEmitter, min_speed, max_speed);
+			randomEmitterFrequency, this, this.ampliEmitter, min_speed,
+			max_speed);
 };
 
 function randomEmitterFrequency(emitter, min_speed, max_speed) {
-	emitter.frequency = this.game.rnd.integerInRange(
-			min_speed, max_speed);
+	emitter.frequency = this.game.rnd.integerInRange(min_speed, max_speed);
+}
+
+function EmitterKillOutOfBound(emitter){
+	emitter.forEach(killParticle, this);
+}
+
+function killParticle(particle) {
+	particle.checkWorldBounds = true;
+	particle.outOfBoundsKill = true;
 }
 
 module.exports = Negaowl;
